3946e9d06c6eef79c1b6b2b45fdb81d8586834ee
Lovepreet Singh <lsingh4all@gmail.com>
2019-05-30 13:42:08 +0530

    Grammer & Constants
    Added Grammer File and TokenType constants are moved from Tokenizer.py to Constant.py File.

--- Constants.py
+++ Constants.py
@@ -2,4 +2,13 @@
 ## CONSTANTS
 ################################################################################
 
-DIGITS      = '0123456789'
+DIGITS              = '0123456789'
+
+TT_INT              = 'INT'
+TT_FLOAT            = 'FLOAT'
+TT_PLUS             = 'PLUS'
+TT_MINUS            = 'MINUS'
+TT_MULTIPLY         = 'MUL'
+TT_DIVIDE           = 'DIV'
+TT_L_PAREN          = 'LPAREN'
+TT_R_PAREN          = 'RPAREN'
--- Tokenizer.py
+++ Tokenizer.py
@@ -2,15 +2,6 @@
 ## TOKENS
 ################################################################################
 
-TT_INT              = 'INT'
-TT_FLOAT            = 'FLOAT'
-TT_PLUS             = 'PLUS'
-TT_MINUS            = 'MINUS'
-TT_MULTIPLY         = 'MUL'
-TT_DIVIDE           = 'DIV'
-TT_L_PAREN          = 'LPAREN'
-TT_R_PAREN          = 'RPAREN'
-
 class Token:
     def __init__(self, a_type, a_value=None):
         self.type = a_type
